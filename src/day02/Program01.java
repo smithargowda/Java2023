f74a3bfd3cae501753d5cf57f1d91945cbd7e660
smithargowda <smithargowda31@gmail.com>
2023-06-26 21:44:42 +0530

    day02
    data types

---
+++ src/day02/Program01.java
@@ -0,0 +1,29 @@
+package day02;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+	boolean a=true;
+	System.out.println(a);
+	int b=10;
+	System.out.println(b);
+	double c=100;
+	System.out.println(c);
+	float d=0.005f;
+	System.out.println(d);
+	byte e=12;
+	System.out.println(e);
+	short f =1289;
+	System.out.println(f);
+	f=e;
+	System.out.println(f);
+	char g='h';
+	System.out.println(g);
+	
+	
+	
+			
+
+	}
+
+}
