6fde209fa478b5928b3225e872bc0166fae64b4c
smithargowda <smithargowda31@gmail.com>
2023-06-27 21:17:52 +0530

    day03- program04
    return type

---
+++ src/day03/Program04.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class Program04 {
+
+	public static void main(String[] args) {
+	int x=addfunction(10,20);
+	System.out.println(x);
+	System.out.print(x*x);
+	
+	}
+	public static int addfunction(int a, int b)
+	{
+		return a+b;
+
+	}
+
+}
